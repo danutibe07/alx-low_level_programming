c5197dfb2a85ab52ce4a6800c77e6fcd25974b8b
danutibe07 <danielutibe07@gmail.com>
2023-06-30 03:47:15 +0100

    Changes to pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,23 @@
 #include "main.h"
-
 /**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
- *
- * Return: void
+ * reverse_array - reverses the content of an array
+ * of integers.
+ * @a: array.
+ * @n: number of elements of the array.
+ * Return: no return.
  */
 
 void reverse_array(int *a, int n)
 {
-	int i;
-	int t;
+	int i, j, temp;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
-
-
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,20 @@
 #include "main.h"
-
 /**
- * string_toupper - change all lowercase to uppercase
- * @n: pointer
- *
- * Return: n
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
-char *string_toupper(char *n)
+char *string_toupper(char *s)
 {
-	int i;
+	int count = 0;
 
-	i = 0;
-	while (n[i] != '\0')
+	while (*(s + count) != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
-		i++;
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
 	}
-	return (n);
-}
 
+	return (s);
+}
